af5f77858c14d0069faf64a3902b2e70c58a1fb7
Soren Rasmussen <s.c.rasmussen@gmail.com>
2018-06-05 18:37:44 -0700

    change this_image to MPI rank

--- src/utilities/assertions_implementation.f90
+++ src/utilities/assertions_implementation.f90
@@ -3,13 +3,17 @@ submodule(assertions_interface) assertions_implementation
 contains
   elemental impure module subroutine assert(assertion,description,success)
     use iso_fortran_env, only : error_unit
+    use mpi_f08, only : MPI_Comm_rank, MPI_COMM_WORLD
     logical, intent(in) :: assertion
     character(len=*), intent(in) :: description
     logical, intent(out), optional :: success
+    integer :: rank, ierr
     if (present(success)) success=assertion
     if (.not.assertion) then
-      write(error_unit,*) 'Assertion "',description,'" failed on image ',this_image()
-      if (.not. present(success)) error stop  
+      call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
+
+      write(error_unit,*) 'Assertion "',description,'" failed on image ', rank
+      if (.not. present(success)) error stop
     end if
   end subroutine
 end submodule
