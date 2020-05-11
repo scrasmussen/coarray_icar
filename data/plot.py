af0c75b8c42d9569cf52a24e935c363a0ea1e489
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-05-11 16:13:31 +0100

    pretty much done with the plotting!

--- data/plot.py
+++ data/plot.py
@@ -27,7 +27,9 @@ field = np.zeros((nx,nz,ny,num_t))
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.set_xlim(0,nx); ax.set_ylim(0,ny); ax.set_zlim(0,nz)
-
+ax.set_xlabel="x axis"
+ax.set_ylabel="y axis"
+ax.set_zlabel="z axis"
 
 
 print("ARTLESS: remove this once real data is produced")
@@ -39,8 +41,6 @@ norm = matplotlib.colors.Normalize(vmin=particles['temperature'].min()-10,
                                    vmax=particles['temperature'].max())
 
 
-
-
 # ---- Set up colors ----
 cmap = plt.cm.Greens
 cmap_c = cmap(norm(particles.temperature.values))
@@ -48,29 +48,35 @@ cmap_c = cmap(norm(particles.temperature.values))
 # ---- 3d plots ----
 # ax.plot3D(particles['x'],particles['y'],particles['z'],
 # normalized = particles['temperature'] / particles['temperature'].max()
-old = ax.scatter3D(particles.iloc[0]['x'], particles.iloc[0]['y'],
-             particles.iloc[0]['z'], marker='X', color=cmap_c[0])
 
-
-t = 1
-t_max = particles['time step'].max()
-
+t = 0
 def updateFig(*args):
     global t, old
-    old.remove()
-    x,y,z = particles.iloc[t-1][['x','y','z']]
-    im = ax.scatter3D(x,y,z, marker='.', color=cmap_c[t-1])
+    if (t == num_t):
+        plt.cla()
+        ax.set_xlim(0,nx); ax.set_ylim(0,ny); ax.set_zlim(0,nz)
+        t = 0
+    if (t != 0):
+        old.remove()
+        x,y,z = particles.iloc[t-1][['x','y','z']]
+        im = ax.scatter3D(x,y,z, marker='.', color=cmap_c[t-1])
     x,y,z = particles.iloc[t][['x','y','z']]
     old = ax.scatter3D(x,y,z, marker='o', color=cmap_c[t])
 
     t += 1
-    return im
+    return old
 
 
 # --- animation ----
 frame_delay_ms=100
 ani = animation.FuncAnimation(fig, updateFig, interval=frame_delay_ms,
-                              frames=t_max-1,repeat=False)
+                              frames=num_t-1,repeat=True)
 
-plt.show()
+gif = True
+gif = False
+if (gif):
+    ani.save('./test.gif', writer='imagemagick', fps=5)
+else:
+    plt.show()
+
 print("Fin!")
