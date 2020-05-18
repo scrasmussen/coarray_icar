92b4528b29effacbd46ed05143f26799c349d1c5
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-05-18 19:40:56 +0100

    can handle multiple particles, better formatting of grpah

--- data/plot.py
+++ data/plot.py
@@ -1,4 +1,5 @@
 from mpl_toolkits import mplot3d
+from matplotlib.ticker import MaxNLocator
 import matplotlib.pyplot as plt
 import matplotlib.colors
 import matplotlib.animation as animation
@@ -23,12 +24,11 @@ header = ['image','timestep','identifier', 'exists','moved', 'x', 'y', 'z',
 particles = pd.read_csv(f, sep='\s+',header=None, names=header)
 num_t = particles['timestep'].max()
 num_particles = list(particles.identifier.unique())
-field = np.zeros((nx,nz,ny,num_t))
+up = particles.identifier.unique()
 
 print("ARTLESS: remove this once real data is produced")
 new_temp = [i+x for i,x in enumerate(particles['temperature'])]
 particles['temperature'] = new_temp
-
 print("ARTLESS: fix normalization")
 norm = matplotlib.colors.Normalize(vmin=particles['temperature'].min()-10,
                                    vmax=particles['temperature'].max())
@@ -37,46 +37,46 @@ cmap = plt.cm.Greens
 cmap_c = cmap(norm(particles.temperature.values))
 
 fig = plt.figure(figsize=plt.figaspect(0.5))
-
 ax = fig.add_subplot(1,2,1,projection='3d')
 ax.set_xlim(0,nx); ax.set_ylim(0,ny); ax.set_zlim(0,nz)
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+ax.zaxis.set_major_locator(MaxNLocator(integer=True))
+
 ax.set_xlabel="x axis"
 ax.set_ylabel="y axis"
 ax.set_zlabel="z axis"
 
 # --- plot image lines ---
 def plot_image_lines():
+    ax.set_title("Particle Movement", y=1.05)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.zaxis.set_major_locator(MaxNLocator(integer=True))
     for i in range (1,ximages):
         ax.plot(xs=[(i*nx)/ximages,(i*nx)/ximages], ys=[0,ny], color='black')
     for i in range (1,yimages):
         ax.plot(xs=[0,nx], ys=[(i*ny)/yimages,(i*ny)/yimages], color='black')
 
 # ---- 3d plots ----
-plot_image_lines()
-temp = fig.add_subplot(2,2,2)
-density = fig.add_subplot(2,2,4)
-temp_max = particles.temperature.max()
+temp        = fig.add_subplot(2,2,2)
+density     = fig.add_subplot(2,2,4)
+temp_max    = particles.temperature.max()
 density_max = particles.density.max()
-temp_max += 10
+temp_max    *= 1.02
+density_max *= 1.1
 
-t = 0
-up = particles.identifier.unique()
-q = particles
 
+# --- set graph limits ---
 def set_graph_lim():
+    temp.set_title("temperature")
     temp.set_xlim(0,num_t)
     temp.set_ylim(0,temp_max)
+    density.set_title("density", y=-0.3)
     density.set_xlim(0,num_t)
     density.set_ylim(0,density_max)
-set_graph_lim()
 
-def updateBarGraphs(ps,i):
-    p = ['particle 1']
-    temp.bar(p,ps.iloc[i].temperature, width=0.5, label='Temp',
-            color='#3CB371')
-    density.bar(p,ps.iloc[i].density, width=0.5,  label='Density',
-            color='#6495ED') #cornflowerblue
-old = []
+# --- function that gets run every animation timestep ---
 def updateFig(*args):
     global t, old
 
@@ -90,16 +90,16 @@ def updateFig(*args):
         t = 0
 
     for id in up:
-        p = particles[ (particles.identifier == id) &
-                       (particles.timestep == t) ]
         p_less_than = particles[ (particles.identifier == id) &
-                                 (particles.timestep <= t) ]
+                                 (particles.timestep < t) ]
         p_density = p_less_than.density
         p_temp    = p_less_than.temperature
         p_time    = p_less_than.timestep
-
         temp.plot(p_time, p_temp, color='black')
         density.plot(p_time, p_density, color='black')
+
+        p = particles[ (particles.identifier == id) &
+                       (particles.timestep == t) ]
         if not p.empty:
             old = ax.scatter3D(p.x, p.y, p.z, marker='o', edgecolor='black',
                                color=cmap_c[t])
@@ -108,17 +108,16 @@ def updateFig(*args):
             density.plot(p_time[-1:], p_density[-1:], color='black',
                          marker='x')
 
-
     t += 1
     return old
 
-        # temp.bar(p,ps.iloc[i].temperature, width=0.5, label='Temp',
-            #          color='#3CB371')
-            # density.bar(p,ps.iloc[i].density, width=0.5,  label='Density',
-            #             color='#6495ED') #cornflowerblue
 
 # --- animation ----
+# - setup graphs
+plot_image_lines()
+set_graph_lim()
 frame_delay_ms=100
+t = 0
 ani = animation.FuncAnimation(fig, updateFig, interval=frame_delay_ms,
                               frames=num_t-1,repeat=True)
 
