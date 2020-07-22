3155539958449f6e64ef1d9dc8540553fa60796a
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-07-22 18:08:40 +0100

    cleaning up files
    Former-commit-id: 5006d0ee1d0b84b25130fe9fc7793890b311916f
    Former-commit-id: 84abbad8446f3eb515e7dae6195ea5425f587ea7
    Former-commit-id: ca37d1d8acf00d75baa80127ec626936a7572d80

--- src/objects/convection_type_implementation.f90
+++
@@ -1,9 +0,0 @@
-submodule(convection_type_interface) convection_type_implementation
-  implicit none
-
-contains
-
-  ! module subroutine test()
-  !   print *, "in test"
-  ! end subroutine test
-end submodule convection_type_implementation
