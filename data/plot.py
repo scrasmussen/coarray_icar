c459af42121986b722ac737ea3a56e5eb4dbbebb
scrasmussen <soren.rasmussen@aggiemail.usu.edu>
2020-07-22 14:30:17 -0700

    cleaning up
    Former-commit-id: 07ee33f999807c64a883bca0c73bb8aad16369ef
    Former-commit-id: cc20ce55990c7f8bfbbcfe9c8b3e5f4c34bd9680
    Former-commit-id: c1d576bd69bf9712e90a671373a42583e59fc9f0

--- data/plot.py
+++ data/plot.py
@@ -12,106 +12,103 @@ if (len(sys.argv) < 2):
     sys.exit("Error: too few arguments for `plot.py [graph_data.txt]`")
 
 
-frame_delay_ms=10
-frame_delay_ms=25
-frame_delay_ms=50
-frame_delay_ms=100
-frame_delay_ms=200
-# frame_delay_ms=300
-
-
-#
+frame_delay_ms=10 # 25
 turn_off_graphs=True
-# turn_off_graphs=False
 
 
 
-# ---- read input data ----
+# ------------------------------------------------------------------------------
+# read input data
+# ------------------------------------------------------------------------------
 f = open(sys.argv[1])
 l = f.readline()
 dims = [int(n) for n in l.split()]
 nx = dims[0]; nz = dims[1]; ny = dims[2]
 ximages = dims[3]; yimages = dims[4]
-
-nx += 1
-ny += 1
-nz += 1
+dz_value = 500
+time=0
+nx += 1; ny += 1; nz += 1
 
 header = ['image','timestep','identifier', 'exists','moved', 'x', 'y', 'z',
-          'u', 'v', 'w', 'k', 'pressure','temperature', 'potential_temperature',
-          'velocity', 'water_vapor','mixing_ratio']
-
-if (f.name == 'four-200-time-step.txt'):
-    header = ['image','timestep','identifier', 'exists','moved', 'x', 'y', 'z',
-          'u', 'v', 'w', 'k', 'pressure','temperature', 'potential_temperature',
-          'velocity','water_vapor']
-
+          'u', 'v', 'w', 'z_meters', 'z_interface', 'pressure','temperature',
+          'potential_temperature', 'velocity', 'water_vapor','cloud_water']
 particles = pd.read_csv(f, sep='\s+',header=None, names=header)
-# sys.exit()
 num_t = particles['timestep'].max()
-num_particles = list(particles.identifier.unique())
 unique_particles = particles.identifier.unique()
+# num_to_keep = int(len(unique_particles) * 0.05)
+# unique_particles = np.random.choice(unique_particles, num_to_keep,
+#                                     replace=False)
+# particles = particles.loc[particles['identifier'].isin(unique_particles)]
+unique_particles = particles.identifier.unique()
+num_particles = list(particles.identifier.unique())
 
-print("ARTLESS: fix normalization")
+
+# ---- Set up colors ----
 norm = matplotlib.colors.Normalize(vmin=particles['temperature'].min()-10,
                                    vmax=particles['temperature'].max())
-# ---- Set up colors ----
-cmap = plt.cm.Greens
+cmap = plt.cm.Blues # cmap = plt.cm.Greens
 cmap_c = cmap(norm(particles.temperature.values))
+blue_cmap = plt.cm.Blues
+blue_cmap.set_under(color='black')
 
+
+# ---- setup graph and plot if needed ----
 fig = plt.figure(figsize=plt.figaspect(0.5))
 if not turn_off_graphs:
     ax = fig.add_subplot(1,2,1,projection='3d')
 else:
     ax = fig.add_subplot(1,1,1,projection='3d')
-ax.set_xlim(1,nx); ax.set_ylim(1,ny); ax.set_zlim(1,nz)
+ax.set_xlim(1,nx); ax.set_ylim(1,ny); ax.set_zlim(1,particles['z_meters'].max())
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 ax.zaxis.set_major_locator(MaxNLocator(integer=True))
 
-ax.set_xlabel="x axis"
-ax.set_ylabel="y axis"
-ax.set_zlabel="z axis"
+ax.set_zlabel("meters")
+ax.set_xticklabels([])
+ax.set_yticklabels([])
 
-# --- plot hill lines ---
-def hill_contour(hillX,hillY):
-    hillZ = np.full_like(hillX,0.0)
-    ids = 1
-    jds = 1
-    ide = nx
-    jde = ny
-    dz_value = 500
-    surface_z = 0
-    n_hills = 1.0
-    hill_height = 1000.0
 
-    hillZ = (np.sin((hillX-ids)/((ide-ids)/n_hills)*2*3.14159-3.14159/2)+1)/2 *\
-            (np.sin((hillY-jds)/((jde-jds)/n_hills)*2*3.14159-3.14159/2)+1)/2
-    hillZ = ((hillZ * hill_height ) + (surface_z + dz_value / 2.0)) / dz_value
-    return hillZ
-
+# --- plot hill contour lines ---
 fineness=1
 hillx = np.linspace(1,nx,nx*fineness)
 hilly = np.linspace(1,ny,ny*fineness)
 hillX,hillY = np.meshgrid(hillx,hilly)
-hillZ = hill_contour(hillX,hillY)
+hillZ = np.full_like(hillX,0.0)
+ids = 1; jds = 1; ide = nx; jde = ny
+surface_z = 0
+n_hills = 1.0
+hill_height = 1000.0
+hillZ = (np.sin((hillX-ids)/((ide-ids)/n_hills)*2*3.14159-3.14159/2)+1)/2 *\
+        (np.sin((hillY-jds)/((jde-jds)/n_hills)*2*3.14159-3.14159/2)+1)/2
+hillZ = ((hillZ * hill_height ) + (surface_z + dz_value / 2.0))
+hillcolor='black' # hillcolor='darkolivegreen'
 
 
 # --- plot image lines ---
-def plot_image_lines():
-    ax.set_title("Particle Movement", y=1.05)
+def plot_image_lines(time):
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.set_title("Air Parcels", y=1.05)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
     ax.zaxis.set_major_locator(MaxNLocator(integer=True))
+    z_labels = ax.get_zticklabels()
+    ax.set_zlabel("meters")
+    ax.set_zticklabels(['0k','1.5k','3k','4.5k','6k','7.5k','9k','10.5k'])
+    ax.grid(b=None)
     for i in range (1,ximages):
         ax.plot(xs=[(i*nx)/ximages,(i*nx)/ximages], ys=[0,ny], color='black')
     for i in range (1,yimages):
         ax.plot(xs=[0,nx], ys=[(i*ny)/yimages,(i*ny)/yimages], color='black')
-    ax.contour3D(hillX,hillY,hillZ, cmap='binary')
+    ax.contour3D(hillX,hillY,hillZ, colors=hillcolor)
+    ax.set_xticks([])
+    ax.set_yticks([])
 
 
+
+# ---- setup additional graphs ----
 if not turn_off_graphs:
-    # --- set up second graph ---
+    # --- setup second graph ---
     second_graph_title  = 'temperature'
     second_graph_entity = second_graph_title.replace(' ', '_')
     second_graph        = fig.add_subplot(2,2,2)
@@ -124,7 +121,7 @@ if not turn_off_graphs:
     second_graph_min   *= 1.1
 
 
-    # --- set up third graph ---
+    # --- setup third graph ---
     # choose type
     third_graph_title = "density"
     third_graph_title = "water vapor"
@@ -155,92 +152,65 @@ def set_graph_lim():
     third_graph.set_ylim(third_graph_min,third_graph_max)
 
 
+# ------------------------------------------------------------------------------
+# graph 3d scatter plot, called every time step
+# ------------------------------------------------------------------------------
+def graph_scatter(p):
+    scatter = ax.scatter3D(p.x, p.y, p.z_meters, marker='o', edgecolor='black',
+                       color='blue')#cmap_c[0])
+    #                  c=p.cloud_water, cmap=blue_cmap, vmin=0.00000)
+    #                  c=p.water_vapor, cmap=blue_cmap, vmin=0.0)
+    return scatter
 
-particles = particles[['timestep','x','y','z']]
+
+
+# --- plot initial timestep ---
+particles = particles[['timestep','x','y','z_meters','water_vapor','cloud_water'
+                       ,'temperature']]
 p = particles[ (particles.timestep == 0) ]
-old = ax.scatter3D(p.x, p.y, p.z, marker='o', edgecolor='black',
-                       color=cmap_c[0])
+scatter = graph_scatter(p)
 
 
-# --- function that gets run every animation timestep ---
+# ------------------------------------------------------------------------------
+#  function that gets run every animation timestep
+# ------------------------------------------------------------------------------
 def updateFig(*args):
-    global t, old
+    global t, scatter, time
 
+    ax.set_title("Particle Movement t="+str(t), y=1.05)
     if (t == num_t):
         ax.cla()
-        ax.set_xlim(1,nx); ax.set_ylim(1,ny); ax.set_zlim(1,nz)
-        plot_image_lines()
+        ax.set_xlim(1,nx); ax.set_ylim(1,ny); ax.set_zlim(1,particles['z_meters'].max())
+        plot_image_lines(time)
         t = 0
     else:
-        old.remove()
+        scatter.remove()
 
     p = particles[ (particles.timestep == t) ]
-    old = ax.scatter3D(p.x, p.y, p.z, marker='o', color='green')
-                       # color=cmap_c[t]) edgecolor='black')
-
+    scatter = graph_scatter(p)
     t += 1
-    return old
-
-    # # old style
-    # if not turn_off_graphs:
-    #     second_graph.cla()
-    #     third_graph.cla()
-    #     # set_graph_lim()
-    # clear_3D_graph = False
-    # if (clear_3D_graph):
-    #     ax.cla()
-    #     ax.set_xlim(0,nx); ax.set_ylim(0,ny); ax.set_zlim(0,nz)
-    #     plot_image_lines()
-    # for id in unique_particles:
-    #     break
-    #     if not turn_off_graphs:
-    #         p_less_than = particles[ (particles.identifier == id) &
-    #                                  (particles.timestep < t) ]
-    #         p_time = p_less_than.timestep
-
-    #         p_second_graph = p_less_than[second_graph_entity]
-    #         second_graph.plot(p_time, p_second_graph, color='black')
-    #         p_third_graph  = p_less_than[third_graph_entity]
-    #         third_graph.plot(p_time, p_third_graph, color='black')
-
-    #     # ax.view_init(10+(t/75), 45+int(t/25))
-
-    #     p = particles[ (particles.identifier == id) &
-    #                    (particles.timestep == t) ]
-    #     if not p.empty:
-    #         old = ax.scatter3D(p.x, p.y, p.z, marker='o', edgecolor='black',
-    #                            color=cmap_c[t])
-    #     else:
-    #         if not turn_off_graphs:
-    #             second_graph.plot(p_time[-1:], p_second_graph[-1:],
-    #                               color='black', marker='x')
-    #             third_graph.plot(p_time[-1:], p_third_graph[-1:], color='black',
-    #                              marker='x')
-
-    # t += 1
-    # return old
+    return scatter
 
 
-# --- animation ----
-# - setup graphs
-plot_image_lines()
-set_graph_lim()
-
-gif    = True
+# ------------------------------------------------------------------------------
+# setup animation
+# ------------------------------------------------------------------------------
+# decide whether to create gif or not
 gif    = False
 repeat = False if gif else True
 
-# num_t = 4
-# ax.view_init(0, 0)
-
+# - setup graph and start animation
+plot_image_lines(time)
+set_graph_lim()
 t = 0
 ani = animation.FuncAnimation(fig, updateFig, interval=frame_delay_ms,
                               frames=num_t-1,repeat=repeat)
 
+# gif not working on my mac right now
 if (gif):
     print("Gif!")
-    ani.save('./test.gif', writer='imagemagick', fps=None) # fps was 5
+    # ani.save('giftest.gif', writer=animation.PillowWriter, fps=None,dpi=20) # fps was 5
+    # ani.save('giftest.gif', writer=animation.ImageMagickWriter, fps=None) # fps was 5
 else:
     plt.show()
-
 print("Fin!")
