c2d85a93ec8c549e19c04aea2b363564412583c8
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-07-22 18:31:08 +0100

    cleaning up
    Former-commit-id: 55a4756afe98eac459cf48149c68dc786c7602b2
    Former-commit-id: 1daab1c8acd33c243e868ba01c67a9c948f65186
    Former-commit-id: 428c9d108290bd0f199659a1b55dfce8f61969ad

--- src/tests/test-ideal.f90
+++ src/tests/test-ideal.f90
@@ -4,20 +4,34 @@ program main
   use assertions_interface, only : assert
   use module_mp_driver, only: microphysics
   use timer_interface, only: timer_t
+  use convection_exchangeable_interface, only : num_particles
   implicit none
 
-  if (this_image()==1) print *,"Number of images = ",num_images()
-
+  integer :: me
+  me = this_image()
+  if (me==1) print *,"Number of images = ",num_images()
 
   block
     type(domain_t), save :: domain
-    integer :: i,nz, ypos,xpos
+    integer, parameter :: timesteps = 500
+    logical :: report = .true.
+    logical, parameter :: convection = .true.
+    logical, parameter :: print_timestep = .false.
+
+    integer :: i,nz, ypos,xpos, n_particles
     type(timer_t) :: timer
+    character*32 hostname
 
-    if (this_image()==1) print *,this_image(),"domain%initialize_from_file('input-parameters.txt')"
-    call domain%initialize_from_file('input-parameters.txt')
+    if (convection .eqv. .false.) report = .false.
 
-    if (this_image()==1) then
+    ! call get_environment_variable('HOSTNAME',hostname)
+    ! print *, me, ': has hostname ', trim(hostname)
+    ! sync all
+
+    if (me==1) print *,me,"domain%initialize_from_file('input-parameters.txt')"
+    call domain%initialize_from_file('input-parameters.txt', convection)
+
+    if (me==1) then
         nz = size(domain%pressure,2)
         print *, " Layer height       Pressure        Temperature      Water Vapor"
         print *, "     [m]              [hPa]             [K]            [kg/kg]"
@@ -31,40 +45,55 @@ program main
 
     ! initialize microphysics before starting the timer
     call microphysics(domain, dt = 20.0)
-    if (this_image()==1) print*, ""
-    if (this_image()==1) print*, "Beginning simulation..."
+
+    if (me==1) print*, ""
+    if (me==1) print*, "Beginning simulation..."
+
+    if (report .eqv. .true.) call domain%report_convection(0)
+
+
     sync all
     call timer%start()
-    do i=1,200
-        ! print *,"Microphysics"
+    do i=1, timesteps
+        if ((print_timestep .eqv. .true.) .and. me==1) then
+          print *, &
+              "________________________timestep ", i,"________________________"
+        end if
         ! note should this be wrapped into the domain object(?)
-        call microphysics(domain, dt = 20.0, halo=1)
+        call microphysics(domain, dt = 20.0, halo=1, &
+            convected_particles = convection)
         call domain%halo_send()
-        call microphysics(domain, dt = 20.0, subset=1)
+        call microphysics(domain, dt = 20.0, subset=1, &
+            convected_particles = convection)
+        ! call domain%halo_send()
+        call domain%halo_retrieve(convection)
 
-        call domain%halo_retrieve()
+        if (report .eqv. .true.) call domain%report_convection(i)
 
         call domain%advect(dt = 1.0)
-
-        ! if (this_image()==(num_images()/2)) then
-        !     print*, domain%accumulated_precipitation(::3,ypos)
-        ! endif
-
     end do
     sync all
     call timer%stop()
 
-    if (this_image()==1) then
+    if (me==1) then
+        n_particles = num_particles()
+        print *, "For", timesteps, "timesteps"
+        if (convection .eqv. .true.) then
+            print *, "With", n_particles, "particles per image for a total of", &
+                n_particles * num_images()
+        else
+            print *, "With no particles"
+        end if
         print *,"Model run time:",timer%as_string('(f8.3," seconds")')
     endif
 
     ypos = (domain%jde-domain%jds)/2 + domain%jds
     do i=1,num_images()
         sync all
-        if (this_image()==i) then
+        if (me==i) then
             if ((ypos>=domain%jts).and.(ypos<=domain%jte)) then
                 xpos = (domain%ite-domain%its)/2 + domain%its
-                print*, this_image(), " : ", domain%accumulated_precipitation(domain%its:domain%ite:2,ypos)
+                ! print*, me, " : ", domain%accumulated_precipitation(domain%its:domain%ite:2,ypos)
             endif
         endif
     enddo
@@ -72,6 +101,6 @@ program main
   end block
 
 
-  if (this_image()==1) print *,"Test passed."
+  if (me==1) print *,"Test passed."
 
 end program
