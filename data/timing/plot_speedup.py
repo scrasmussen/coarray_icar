99d84dbf7c79d90c7dc45c4f298979078a62eebc
scrasmussen <soren.rasmussen@aggiemail.usu.edu>
2020-08-10 13:38:10 -0700

    changes
    Former-commit-id: d199597a71ab933c6072e9ba7a55569193a61109
    Former-commit-id: 1704c0aaeff4d624509562dce9bb088b1afd3c0d
    Former-commit-id: 7d5a346762188b703454d224f40e183f794542b1

---
+++ data/timing/plot_speedup.py
@@ -0,0 +1,70 @@
+from mpl_toolkits import mplot3d
+from matplotlib.ticker import MaxNLocator
+import matplotlib.pyplot as plt
+import matplotlib.colors
+import matplotlib.animation as animation
+import numpy as np
+import pandas as pd
+import sys
+
+
+if (len(sys.argv) < 2):
+    sys.exit("Error: too few arguments for `plot.py [graph_data.txt]`")
+
+# ---- read input data ----
+f = open(sys.argv[1])
+header = ['nx','nz','ny','np','x_images','y_images','n_particles','timesteps',
+          'time']
+df = pd.read_csv(f, sep='\s+',header=None, names=header)
+
+
+# --- animation ----
+gif    = True
+gif    = False
+repeat = False if gif else True
+
+# ax.scatter(particles.timestep, df.time,
+#            cmap=discrete_cmap, c=particles.identifier)
+#            marker='.', edgecolor='black')
+#            color=cmap_c[0])
+
+# --- setup colormap ---
+discrete_cmap = plt.get_cmap('tab20b')
+
+# --- plot data ---
+for i,size in enumerate(df.nx.unique()):
+    l = df.loc[df.nx == size, ['nx','ny','nz']].iloc[0]
+    label = l.to_csv(header=False, index=False).replace('\n','x')[:-1]
+
+    data = df[df.nx == size]
+    t1 = (data.loc[data.np == 1].time.values[0])
+
+    # speedup = t_1 / t_n
+    speedup = t1 / data.time
+
+    plt.plot(df[df.nx == size].np, speedup, marker = '.',
+             label=label)
+             # color=discrete_cmap(i*4))
+
+plt.legend(title="Dimensions")
+plt.xlabel("number of images")
+plt.ylabel("speedup")
+plt.title("Speedup")
+
+# plt.xscale('log', basex=2)
+# ax.set_xticklabels([])
+# ax.set_yticklabels([])
+
+
+# print(len(ax.get_yticklabels()))
+# ax.get_xaxis().set_visible(False)
+# ax.get_yaxis().set_visible(False)
+# sys.exit()
+
+if (gif):
+    print("Gif!")
+    # ani.save('test.gif', writer=animation.PillowWriter, fps=None,dpi=20) # fps was 5
+    # ani.save('test.gif', writer=animation.ImageMagickWriter, fps=None) # fps was 5
+else:
+    plt.show()
+print("Fin!")
