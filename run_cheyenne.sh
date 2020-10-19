63342dfbcfc7d07238208d4b7362b7df4656ff6e
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-10-19 10:29:54 -0700

    should be complete
    Former-commit-id: 799ab017cb55ed7440f3b717fd3cfd1aeea87914
    Former-commit-id: e7c1e4c8beec9724b0936d21626bf441758817df
    Former-commit-id: e4dc6ac125943342076d0f8e34ec6614564b6a09

--- run_cheyenne.sh
+++ run_cheyenne.sh
@@ -8,40 +8,67 @@
 #PBS -l walltime=01:00:00
 #PBS -l select=1:ncpus=32:mpiprocs=32
 
+set -e  # stop running on error
 
 # --- fill in parameters ---
-nodes=1
-strong_scaling=false
+nodes=2
+strong_scaling=true
 weak_scaling=true
 exe=./test-ideal
 # --- parameters automatically calculated ---
 n=$((${nodes} * 18))
+echo "--- VARIABLES CHOSEN ---"
+echo " nodes=${nodes}"
+echo " processes=${n}"
+echo " strong scaling=${strong_scaling}"
+echo " weak scaling=${weak_scaling}"
+echo " executable=${exe}"
+echo "------------------------"
+
 
 
 # --- define user functions ---
 file=input-parameters.txt
 function create_input {
-    rm -f $file
-    echo "&grid nx=$1,ny=$2,nz=30 /" > $file
-    echo "" >> $file
-    echo "&options preferred_ratio=1/" >> $file
-    cat $file
+    rm -f ${file}
+    echo "&grid nx=$1,ny=$2,nz=30 /" > ${file}
+    echo "" >> ${file}
+    echo "&options preferred_ratio=1/" >> ${file}
+    echo "created ${1}x${2}x30 parameter file"
+}
+
+function run_n_images {
+    echo "mpiexec -np ${1} ${exe}"
+    mpiexec -np ${1} ${exe}
 }
 
 
-set -e
 
-# ---- strong scaling ----
+# ---- run strong scaling ----
 if [[ "$strong_scaling" = true ]]; then
+    echo "- running strong scaling tests on ${nodes} nodes"
     if [[ ${nodes} == 1 ]]; then
-	mpiexec -np 1 ${exe}
-	mpiexec -np 2 ${exe}
-	mpiexec -np 4 ${exe}
-	mpiexec -np 8 ${exe}
-	mpiexec -np 16 ${exe}
-	mpiexec -np 36 ${exe}
+	create_input 500 500
+	run_n_images 1
+	run_n_images 2
+	run_n_images 4
+	run_n_images 8
+	run_n_images 16
+	run_n_images 36
+
+	create_input 2000 2000
+	run_n_images 1
+	run_n_images 2
+	run_n_images 4
+	run_n_images 8
+	run_n_images 16
+	run_n_images 36
+
     else
-	mpiexec -np ${n} ${exe}
+	create_input 500 500
+	run_n_images ${n}
+	create_input 2000 2000
+	run_n_images ${n}
     fi
 fi
 #
@@ -59,16 +86,52 @@ fi
 # ---- end strong scaling ----
 
 
-
-# ---- weak scaling ----
+# ---- run weak scaling ----
 if [[ "$weak_scaling" = true ]]; then
+    echo "- running weak scaling tests on ${nodes} nodes"
     if [[ ${nodes} == 1 ]]; then
-	mpiexec -np 1 ${exe}
-	mpiexec -np 36 ${exe}
-    else
-	mpiexec -np ${n} ${exe}
+	# 12k per image
+	create_input 20 20
+	run_n_images 1
+	create_input 120 120
+	run_n_images 36
+	# 768k per image
+	create_input 160 160
+	run_n_images 1
+	create_input 960 960
+	run_n_images 36
+    elif [[ ${nodes} == 2 ]] ; then
+	# 12000 per image
+	create_input 160 180
+	run_n_images ${n}
+	# 768k per image
+	create_input 1280 1440
+	run_n_images ${n}
+    elif [[ ${nodes} == 5 ]] ; then
+	# 12000 per image
+	create_input 250 288
+	run_n_images ${n}
+	# 768k per image
+	create_input 2250 2048
+	run_n_images ${n}
+    elif [[ ${nodes} == 10 ]] ; then
+	# 12000 per image
+	create_input 375 384
+	run_n_images ${n}
+	# 768k per image
+	create_input 3000 3072
+	run_n_images ${n}
+    elif [[ ${nodes} == 20 ]] ; then
+	# 12000 per image
+	create_input 500 576
+	run_n_images ${n}
+	# 768k per image
+	create_input 4096 4500
+	run_n_images ${n}
     fi
 fi
+
+# Problem size table for weak scaling
 #
 # 12000 per image
 # | Nodes | Num Images |   x |   y |  z |
@@ -78,7 +141,6 @@ fi
 # |     5 |        180 | 250 | 288 | 30 |
 # |    10 |        360 | 375 | 384 | 30 |
 # |    20 |        720 | 500 | 576 | 30 |
-
 
 # 768000 per image
 # | Nodes | Num Images |    x |    y |  z |
