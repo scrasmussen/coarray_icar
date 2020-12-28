794391f76d46059c787a187ca1b385e561c91e20
Soren Rasmussen <s.c.rasmussen@gmail.com>
2020-12-28 08:45:02 -0800

    added another way to calc BV freq

--- data/brunt_vaisala/brunt_vaisala.py
+++ data/brunt_vaisala/brunt_vaisala.py
@@ -14,7 +14,7 @@ from scipy import fftpack
 
 
 present = False
-present = True
+# present = True
 
 
 
@@ -90,19 +90,29 @@ for id in particles.identifier.unique():
     # theta = particles[particles.identifier == id].potential_temperature.values
     # theta = particles[particles.identifier == id].temperature.values
 
+    # print(theta)
+    # print("-------")
+
     dz = np.diff(z)
+    # -------- N^2_d = (g / theta)   (dtheta / dz) ---------
+    if (1 == g):
+        dth = np.diff(theta)  # new
+        dth_dz = dth / dz
+        n2 = (g / theta[1:]) * dth_dz # new
 
     # -------- N^2_d = g* (dln(theta) / dz) ---------
-    # if (relative_humidity == False):
-    dln = np.diff(np.log(theta))
-    dln_dz = dln / dz
-    n2 = g * dln_dz
+    else:
+        dln = np.diff(np.log(theta))
+        dln_dz = dln / dz
+        n2 = (g ) * dln_dz
+
 
     # -------- N^2_m = g/T (dT/Dz + gamma_m)(1+Lq_s/RT) - g/(1-q_w) dq_w/dz ----
     # else:
 
     # n = n2
     n = np.sqrt(n2)
+
 
     bv.n = n
     bv.n2 = n2
@@ -145,17 +155,18 @@ ax.set_ylabel("env. potential temp. (K)")
 # ax.set_xlabel("elevation (km)")
 # ax.set_ylabel("temperature (K)")
 
-# -----plot pressure-----
-ax2 = fig.add_subplot(rows,cols,2)
-# bdf = particles[['timestep', 'n']]
-ax2.scatter(particles.timestep, particles.n,
-            # ax2.scatter(particles.z_meters, particles.pressure,
-            cmap=discrete_cmap, c=particles.identifier, marker='.')
-ax2.set_ylabel("Brunt-Vaisala freq.")
-ax2.set_ylim(0)
-# ax2.set_xlabel("timesteps")
-# plt.setp(ax2,yticklabels=[])
-# plt.setp(ax2.get_yticklabels(), visible=False)
+# -----plot Brunt-Vaisala Freq-----
+if (0 == 1):
+    ax2 = fig.add_subplot(rows,cols,2)
+    ax2.scatter(particles.timestep, particles.n,
+                cmap=discrete_cmap, c=particles.identifier, marker='.')
+    ax2.set_ylabel("Brunt-Vaisala freq.")
+    ax2.set_ylim(0)
+else:
+    ax2 = fig.add_subplot(rows,cols,2)
+    ax2.scatter(particles.timestep, particles.potential_temperature,
+                cmap=discrete_cmap, c=particles.identifier, marker='.')
+    ax2.set_ylabel("potential temp. (K)")
 
 
 
@@ -247,33 +258,50 @@ if (present == True):
     sys.exit()
 
 
+
+
+# bv_all[bv_all.identifier==1].n.median()
+
 # ---- plot table ----
 t = particles[particles.identifier == 1].timestep.to_numpy()
 
 ax5 = fig.add_subplot(3,1,3)
-t_x = 3
+t_x = 5
 t_y = 1
 table_dim = (t_x,t_y)
 table_data = np.zeros(table_dim, dtype=float)
 for row in range(1,num_particles+1):
-    x = particles[particles.identifier == row].temperature.to_numpy()
+    x = particles[particles.identifier == row].env_potential_temperature.to_numpy()
     x = x[~np.isnan(x)]
     x = x - x[0]  # center data
     period = np.diff(np.where(np.diff(np.sign(x)) < 0))
     ave_period = np.average(np.diff(np.where(np.diff(np.sign(x)) < 0)))
     ave_freq = 1 / ave_period
+    # bv radians per second
+    bv_freq_rad = bv_all[bv_all.identifier==row].n2.median()
+    print("bv = ", bv_freq_rad)
+    bv_freq = bv_freq_rad * 0.159155
     # ave_freq = ave_freq**2
-
+    percent_diff = (abs(ave_freq - bv_freq)/ bv_freq) * 100
 # np.diff(np.where(np.diff(np.sign(x)))[0][:-1])
 
     X = fftpack.fft(x)
     freqs = fftpack.fftfreq(len(x))
     # print(table_data)
+    print(freqs.min(), freqs.max())
+    idx = np.argmax(np.abs(x))
+    freq = freqs[idx]
+    # freq_in_hertz = abs(freq * fra)
 
     # table_data = np.c_[table_data, ['{:,.5f}'.format(ave_freq), ave_period, '']]
-    table_data = np.c_[table_data, ['{:,.5f}'.format(ave_freq),
-                                    '{:,.2f}'.format(ave_period),
+    table_data = np.c_[table_data, ['{:,.2f}'.format(ave_period),
+                                    '{:,.5f}'.format(bv_freq),
+                                    '{:,.5f}'.format(ave_freq),
+                                    '{:,.5f}'.format(percent_diff),
                                     '']]
+                                    # '{:,.5f}'.format(abs(freqs).mean()),
+
+
 
 # sys.exit()
 table_data = table_data[:,1:]
@@ -281,7 +309,9 @@ table_data = table_data[:,1:]
 
 
 # sys.exit()
-col_names = ['ave. frequency','ave. period','particle color']
+col_names = ['period','b.v. freq. (Hz)','freq. (Hz)','% diff.',
+'particle color']
+
 table_data = table_data.transpose()
 table_df = pd.DataFrame(data=table_data, columns=col_names)
 table_df.index += 1
@@ -293,7 +323,7 @@ ax5.axis("off")
 
 table_colors = np.chararray((t_y + 2,num_particles))
 for particle in range(0,num_particles):
-    c_table.get_celld()[(3,particle)].set_color(matplotlib.colors.to_hex(sc.to_rgba(particle+1)))
+    c_table.get_celld()[(t_x,particle)].set_color(matplotlib.colors.to_hex(sc.to_rgba(particle+1)))
     # table_colors[2][particle] =
     # table_colors[2][particle] = matplotlib.colors.to_hex(sc.to_rgba(particle+1))
     # # print(matplotlib.colors.to_rgba(matplotlib.colors.to_hex(sc.to_rgba(particle))))
@@ -307,6 +337,7 @@ if (relative_humidity == False):
     title_end = "dry air parcels"
 
 title = "Brunt-Vaisala Frequency \n"
+title += "Dry air parcels \n"
 title += str(num_particles) + " particles, " + str(num_t) + " timesteps"
 # title += ", " + f.name.split('/')[1]
 plt.suptitle(title)
