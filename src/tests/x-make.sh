ad5b7f4b45ce03618a1bc3fc333f207c859022da
Soren Rasmussen <s.c.rasmussen@gmail.com>
2021-01-20 18:54:56 -0800

    adding nvidia to Makefile, cmake doesn't seem to handle the new compiler

--- src/tests/x-make.sh
+++ src/tests/x-make.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-COMPILER=gnu \
-				make \
-				USE_ASSERTIONS=.false.
+
+# --- gnu ---
+# COMPILER=gnu \
+# 				make \
+# 				USE_ASSERTIONS=.false.
+
+# --- nvidia ---
+COMPILER=nvidia \
+LOOP=DO_LOOP \
+USE_ASSERTIONS=.false. \
+				make test-ideal
