da22cd01c63228b44a11286f68494aa94e5219b6
Soren Rasmussen <s.c.rasmussen@gmail.com>
2021-01-27 15:02:17 -0800

    edits to build Makefile with nvidia

--- src/tests/x-make.sh
+++ src/tests/x-make.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+set -e -x
 
 # --- gnu ---
 # COMPILER=gnu \
 # 				make \
 # 				USE_ASSERTIONS=.false.
 
+# loop options are DO, DC, OMP
+loop=OMP
+
 # --- nvidia ---
 COMPILER=nvidia \
-LOOP=DO_LOOP \
+LOOP=${loop}_LOOP \
 USE_ASSERTIONS=.false. \
+DEBUGVAL=.false. \
 				make test-ideal
+cp test-ideal run/test-ideal-${loop}
